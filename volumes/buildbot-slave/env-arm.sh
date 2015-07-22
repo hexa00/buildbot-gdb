37441d24cf7f01aeb06666344e94269de22db0c8
Antoine Tremblay <antoine.tremblay@ericsson.com>
2015-07-22 09:35:50 -0400

    Added docker file and env for arm build slave

---
+++ volumes/buildbot-slave/env-arm.sh
@@ -0,0 +1,1 @@
+export ICECC_VERSION=x86_64:/ice-arm-cross-compiler.tar.gz
--- volumes/buildbot-slave/run.sh
+++ volumes/buildbot-slave/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 source env.sh
+if [ $1 = "arm" ]; then
+source env-arm.sh
+fi	 
 /etc/init.d/iceccd start
 ccache -F 0
 ccache -M 10G
